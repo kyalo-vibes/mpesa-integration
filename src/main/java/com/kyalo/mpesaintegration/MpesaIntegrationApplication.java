6501e289a369fc4149991d120bf1bb02bd51f12d
Kyalo <kevin.kioko4@gmail.com>
2024-10-24 19:23:53 +0300

    Project set up

---
+++ src/main/java/com/kyalo/mpesaintegration/MpesaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kyalo.mpesaintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpesaIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpesaIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyalo/mpesaintegration/MpesaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyalo.mpesaintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpesaIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
